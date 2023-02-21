e8ef6127c944f74dd1c2fe1f1825b4a007debdc2
Trevor Dunlap <tjdunlap1899@gmail.com>
2023-02-21 11:55:36 -0500

    commit tree 1aa07ff3b55fff4b7373e8e3945693d45faa2ff3

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(event){
+    const heading = document.querySelector("p");
+    heading.textContent = "This is really cool!";
+})
